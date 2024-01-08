7b6c1aa494be25e850a2c9499109d47dc3a50f5a
Temitope Ilesanmi <topephysics@gmail.com>
2024-01-08 00:35:47 +0000

    add cors support for web

--- app.py
+++ app.py
@@ -1,40 +1,46 @@
 from find_actor import find_actor_by_image, find_actor_profile, find_actor_id
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+#  TODO: Enable CORS for allowed origins to enable web support.
+# CORS(app, resources={r"/*": {"origins": "*"}})
 
 
 @app.route("/status", methods=["GET"])
 def get_status():
     return jsonify({"message": "Hello world"})
 
+
 @app.route("/search", methods=["GET"])
 def search():
-   actor_name = request.args.get('name', default='')
-   actor_id = find_actor_id(actor_name)
-   if actor_id is not None:
-    actor_profile = find_actor_profile(actor_id)
-    return jsonify(actor_profile)
-   else:
-    return jsonify({"error": "Actor not found"}), 404
+    actor_name = request.args.get("name", default="")
+    actor_id = find_actor_id(actor_name)
+    if actor_id is not None:
+        actor_profile = find_actor_profile(actor_id)
+        return jsonify(actor_profile)
+    else:
+        return jsonify({"error": "Actor not found"}), 404
 
-@app.route("/rekognise", methods =["POST"])
+
+@app.route("/rekognise", methods=["POST"])
 def rekognise():
-    if 'image' not in request.files:
+    if "image" not in request.files:
         return jsonify({"error": "No file part"}), 400
 
-    file = request.files['image']
+    file = request.files["image"]
 
-    if file and file.filename != '':
+    if file and file.filename != "":
         actor_name = find_actor_by_image(file)
         if actor_name is not None:
             return jsonify({"name": actor_name})
         else:
             return jsonify({"error": "Actor not found"}), 404
     else:
-       # If the user does not select a file, the browser submits an empty file without a filename.
-       return jsonify({"error": "No selected file"}), 400
-        
-        
+        # If the user does not select a file, the browser submits an empty file without a filename.
+        return jsonify({"error": "No selected file"}), 400
+
+
 if __name__ == "__main__":
     app.run(debug=True)
